133a0035d75cd35c1c245b3f90a802bc8416f3dc
Aakash Kumar <kumaraakash77396@gmail.com>
2026-07-31 20:16:48 +0530

    PORT changed

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 
-const PORT = 5050;
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
